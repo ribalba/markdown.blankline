b32999fc105af718509d1642d31ef5ae3895492c
Didi Hoffmann <didi@rebelproject.org>
2014-02-28 15:01:14 +0100

    Added a the install note

--- MarkdownBlankLine/blankline.py
+++ MarkdownBlankLine/blankline.py
@@ -1,4 +1,4 @@
-"""Blank line extension for Markdown.
+"""Html Blank line extension for Markdown.
 
 This extension adds the syntax for an user to add a blank line. This can be in the middle of a text block. 
 Just type %% and it will add a <br>. This is especially useful for adding blank lines on their own. 
